8f5b0e9aa4f8c9218e84ccbe6e05e428fb624916
Sriyansh srivastava <84917748+Sriyansh2001@users.noreply.github.com>
2023-05-16 23:16:47 +0530

    Add files via upload

--- Text Utils/src/Components/Navbar.js
+++ Text Utils/src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React , {useEffect}  from 'react';
 // import {Link} from "react-router-dom";
 
 export default function Navbar(props) {
@@ -11,28 +11,34 @@ export default function Navbar(props) {
   return (
     <div>
       <nav className='nav'>
-        <span className='switchtextarea'>
-          Enable {props.mode=="Dark"?"Light":"Dark"} mode
-        </span>
-        <span className='switcharea'>
-          <input type='checkbox' onChange={props.toggle_visiblity_mode} className='switch' id='switch'></input>
-        </span>
-        <span className='switcharea'>
-          <select className="select_option" id="select_option">
-            <option>Blue</option>
-            <option>Green</option>
-            <option>Red</option>
-            <option>Purple</option>
-          </select>
-          <button onClick={make_change}>Make Theme</button>
-        </span>
-        <span className='list'>
-          <b style={{fontSize:"18px"}}>Text Utils</b> &nbsp;
-          <a href='/' className='item'>Home</a>
-          <a href='#' className='item'>Contact</a>
-          <a href='#' className='item'>Helpline</a>
-          <a href='/about' className='item'>About</a>
-        </span>
+        <div className='nav-menu'>
+          <div className='dark_mode_class_text'>
+            <strong>Enable {props.mode==="Dark"?"Light":"Dark"} mode</strong>
+          </div>
+          <div className='dark_mode_class'>
+            <input type='checkbox' onChange={props.toggle_visiblity_mode} className='switch' id='switch'></input>
+          </div>
+          <div className='switcharea'>
+            <select className="select_option" id="select_option">
+              <option>Blue</option>
+              <option>Green</option>
+              <option>Red</option>
+              <option>Purple</option>
+            </select>
+            <button className='Theme' onClick={make_change}>Change Theme</button>
+          </div>
+
+          <div className='list'>
+            <b style={{fontSize:"18px"}}>Text Utils</b> &nbsp;
+            <a href='/' className='item'>Home</a>
+            <a href='/' className='item'>Contact</a>
+            <a href='/' className='item'>Helpline</a>
+            <a href='/' className='item'>About</a>
+          </div>
+        </div>
+          {/* <button className="Hamburger" onClick={Slide_bar()}>
+          X
+        </button> */}
       </nav>
     </div>
   )
